14b7c27b59f6b5c436ee54597ab17631398f2299
AnnoyingTalkingMachine <colbot001@gmail.com>
2023-06-09 22:54:53 +0700

    init, following xen42 guidance

---
+++ VietnameseTranslation/VietnameseTranslation.cs
@@ -0,0 +1,35 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+
+namespace VietnameseTranslation
+{
+    public class VietnameseTranslation : ModBehaviour
+    {
+        private void Awake()
+        {
+            // You won't be able to access OWML's mod helper in Awake.
+            // So you probably don't want to do anything here.
+            // Use Start() instead.
+        }
+
+        public static VietnameseTranslation Instance;
+
+        private void Start()
+        {
+            var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
+            api.RegisterLanguage(this, "Vietnamese", "assets/Translation.xml");
+            api.AddLanguageFont(this, "Space Mono", "assets/spacemono-regular", "Assets/SpaceMono-Regular.ttf");
+
+            //// Starting here, you'll have access to OWML's mod helper.
+            //ModHelper.Console.WriteLine($"My mod {nameof(VietnameseTranslation)} is loaded!", MessageType.Success);
+
+
+            //// Example of accessing game code.
+            //LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            //{
+            //    if (loadScene != OWScene.SolarSystem) return;
+            //    ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            //};
+        }
+    }
+}
