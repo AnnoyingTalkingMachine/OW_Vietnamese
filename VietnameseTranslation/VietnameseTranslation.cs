28fcbc17a52a2ef43ade078f20e4633dfa0460ca
AnnoyingTalkingMachine <colbot001@gmail.com>
2023-06-11 09:08:24 +0700

    Code buggy, but translation is doing good

--- VietnameseTranslation/VietnameseTranslation.cs
+++ VietnameseTranslation/VietnameseTranslation.cs
@@ -1,11 +1,27 @@
 ﻿using OWML.Common;
 using OWML.ModHelper;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace VietnameseTranslation
 {
     public class VietnameseTranslation : ModBehaviour
     {
+        public static VietnameseTranslation Instance;
+
+        private AssetBundle bundle;
+        public AssetBundle Bundle
+        {
+            get
+            {
+                if (bundle == null)
+                {
+                    bundle = ModHelper.Assets.LoadBundle("assets/font");
+                }
+                return bundle;
+            }
+        }
+
         private void Awake()
         {
             // You won't be able to access OWML's mod helper in Awake.
@@ -13,37 +29,63 @@ namespace VietnameseTranslation
             // Use Start() instead.
         }
 
-        public static VietnameseTranslation Instance;
-
         private void Start()
         {
             var api = ModHelper.Interaction.TryGetModApi<ILocalizationAPI>("xen.LocalizationUtility");
             api.RegisterLanguage(this, "Vietnamese", "assets/VietnameseTranslation.xml");
+
+            Instance = this;
+            //ModHelper.HarmonyHelper.AddPrefix<NomaiTranslatorProp>("InitializeFont", typeof(VietnameseTranslation), nameof(VietnameseTranslation.InitTranslatorFont));
+            ModHelper.HarmonyHelper.AddPrefix<TextTranslation>("GetFont", typeof(VietnameseTranslation), nameof(VietnameseTranslation.GetFont));
             //api.AddLanguageFont(this, "Space Mono", "assets/spacemono-regular", "Assets/SpaceMono-Regular.ttf");
 
-            private AssetBundle bundle;
-            public AssetBundle Bundle
+
+
+
+            //// Starting here, you'll have access to OWML's mod helper.
+            //ModHelper.Console.WriteLine($"My mod {nameof(VietnameseTranslation)} is loaded!", MessageType.Success);
+
+
+            //// Example of accessing game code.
+            //LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
+            //{
+            //    if (loadScene != OWScene.SolarSystem) return;
+            //    ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            //};
+        }
+
+        private static bool InitTranslatorFont(
+            ref Font ____fontInUse,
+            ref Font ____dynamicFontInUse,
+            ref float ____fontSpacingInUse,
+            ref Text ____textField)
+        {
+            ____fontInUse = VietnameseTranslation.self.NotoSansTcMed;
+            ____dynamicFontInUse = VietnameseTranslation.self.NotoSansTcMedDyn;
+            ____fontSpacingInUse = TextTranslation.GetDefaultFontSpacing();
+            ____textField.font = ____fontInUse;
+            ____textField.lineSpacing = ____fontSpacingInUse;
+            return false;
+        }
+
+        private static bool GetFont(
+            bool dynamicFont,
+            ref Font __result)
+        {
+            if (TextTranslation.Get().GetLanguage() != TextTranslation.Language.CHINESE_SIMPLE)
             {
-                get
-                {
-                    if (bundle == null)
-                    {
-                        bundle = ModHelper.Assets.LoadBundle("assets/space");
-                    }
-                    return bundle;
-                }
+                return true;
             }
 
-        //// Starting here, you'll have access to OWML's mod helper.
-        //ModHelper.Console.WriteLine($"My mod {nameof(VietnameseTranslation)} is loaded!", MessageType.Success);
-
-
-        //// Example of accessing game code.
-        //LoadManager.OnCompleteSceneLoad += (scene, loadScene) =>
-        //{
-        //    if (loadScene != OWScene.SolarSystem) return;
-        //    ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
-        //};
-    }
+            if (dynamicFont)
+            {
+                __result = VietnameseTranslation.Instance.NotoSansTcMedDyn;
+            }
+            else
+            {
+                __result = VietnameseTranslation.Instance.NotoSansTcMed;
+            }
+            return false;
+        }
     }
 }
